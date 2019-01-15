f6c3f8f61eaa15a426b8c6279c046b04d72644d9
Raypea <batrob@sfr.fr>
2019-01-15 11:43:12 +0100

    Ajout du formulaire de connexion au site

---
+++ view/frontend/connexion.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<br>
+<div class="container">
+    <form action="" method="post">
+        <h2>Se connecter</h2>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <input type="text" id="login" name="login" class="form-control" placeholder="Votre pseudo">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <input type="password" id="password" name="password" class="form-control" placeholder="Votre mot de passe">
+            </div>
+        </div>
+    </form>
+</div>
