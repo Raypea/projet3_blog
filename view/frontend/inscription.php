587b35640581ae16f74f7addafa70fc98811244c
Raypea <batrob@sfr.fr>
2019-01-15 11:03:52 +0100

    Update du formulaire d'inscription

--- view/frontend/inscription.php
+++ view/frontend/inscription.php
@@ -1,11 +1,28 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <div class="container">
     <form action="" method="post">
+        <h2>Formulaire d'inscription</h2>
         <div class="form-row">
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Prénom">
+            <div class="form-group col-md-6">  
+                <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Prénom">
             </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Nom">
+            <div class="form-group col-md-6">
+                <input type="text" id="lastname" name="lastname" class="form-control" placeholder="Nom">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-12">
+                <input type="text" id="email" name="email" class="form-control" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-12">
+                <input type="text" id="password" name="password" class="form-control" placeholder="Mot de passe">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-12">
+                <input type="text" id="password" name="password" class="form-control" placeholder="Confirmer le mot de passe">
             </div>
         </div>
     </form>
