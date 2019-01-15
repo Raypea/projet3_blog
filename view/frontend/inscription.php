493068b0cebede42eb96e268dd0e6d7dc36bdce3
Raypea <batrob@sfr.fr>
2019-01-15 10:19:39 +0100

    Création de la vue du formulaire d'inscription

---
+++ view/frontend/inscription.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <form action="" method="post">
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Prénom">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Nom">
+            </div>
+        </div>
+    </form>
+</div>
