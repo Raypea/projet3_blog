e271a38fd52b0f4aa3f9055f287d37d794fab6f8
Raypea <batrob@sfr.fr>
2019-01-17 16:43:12 +0100

    Ajout de la page bio de l'auteur

---
+++ include/author.php
@@ -0,0 +1,26 @@
+<div class="container mt-5 mb-5">
+    <div class="row bio-author">
+        <!-- PHOTO AUTEUR -->
+        <div class="col-xl-4">
+            <img class="id-author" src="public/images/author.jpg" alt="Jean Forteroche">
+        </div>
+        <!-- BIOGRAPHIE -->
+        <div class="col-xl-8">
+            <div class="mt-5">
+                <h3 class="text-uppercase">Mon histoire</h3>
+                <p class="text-justify">
+                    Bonjour, je m'appelle Jean Forteroche et je suis écrivain. Je publie mes romans sur mon site en ligne et sur papier.
+                    Je rédige actuellement un nouveau roman intitulé <b>Un billet pour l'Alaska</b>, sous forme de chapitres publiés régulièrement.
+                    N'hésitez pas à poster des commentaires et des critiques à propos de cet ouvrage. Il est important pour moi d'avoir des retours
+                    pour m'améliorer en vue de mes prochains projets.
+                </p>
+
+                <div>
+                    <a class="pr-2" href="#"><i class="fab fa-facebook fa-2x"></i></a>
+                    <a class="pr-2" href="#"><i class="fab fa-twitter fa-2x"></i></a>
+                    <a class="pr-2" href="#"><i class="fab fa-google-plus fa-2x"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
