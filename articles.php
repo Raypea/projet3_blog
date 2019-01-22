c0b6962784874d6d4e70a269ce130714c17d8e43
Raypea <batrob@sfr.fr>
2019-01-22 16:32:12 +0100

    Ajout de la page d'articles

---
+++ articles.php
@@ -0,0 +1,10 @@
+<!-- HEADER PAGE D'ACCUEIL -->
+<?php 
+    $pageTitle = 'Articles - Jean Forteroche';
+    include("include/header.php"); ?>
+
+    <!-- MENU SCROLL AVEC LA PAGE / HEADER -->
+    <?php include("include/nav.php"); ?>
+
+    <!-- FOOTER -->      
+    <?php include("include/footer.php"); ?>
