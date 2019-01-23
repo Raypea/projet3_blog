73d0386ecfd93dab333edf5151c8cc2e045779ff
Raypea <batrob@sfr.fr>
2019-01-23 13:56:27 +0100

    Mise à jour du routeur'

--- routeur.php
+++ routeur.php
@@ -3,36 +3,20 @@
 require('controller/frontoffice.php');
 
 class Router {
-    public function AskController {
-        // Test du paramètre action pour renvoyer vers le bon contrôleur
-        if (isset($_GET['action'])) {
-            if ($_GET['action'] == 'listChapters') {
-                listChapters();
-            }
-            elseif ($_GET['action'] == 'chapter') {
-                if (isset($_GET['id']) && $_GET['id'] > 0) {
-                    chapter();
-                }
-                else {
-                    echo 'Erreur : Aucun identifiant de chapitre envoyé';
-                }
-            }
-            elseif ($_GET['action'] == 'addComment') {
-                if (isset($_GET['id']) && $_GET['id'] > 0) {
-                    if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                        addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-                    }
-                    else {
-                        echo 'Erreur : Certains champs sont vides!';
-                    }
-                }
-                else {
-                    echo 'Erreur : Aucun identifiant de chapitre envoyé';
-                }
-            }
+    public function getPage()
+    {
+        $FOcontroller = new FOController();
+        if(!isset($_GET['action'])) {
+            $FOcontroller->getHome();
+        }
+        else if(isset($_GET['action']) && $_GET['action'] == 'romans')
+        {
+            $FOcontroller->listChapters();
+        }
+        else {
+            header('Location: ./');
         }
     }
-    else {
-        listChapters();
-    }
 }
+
+// 
