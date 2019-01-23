f87a370d505ea9fae3b3c04a0f9bf800bfff4d1f
Raypea <batrob@sfr.fr>
2019-01-23 13:53:41 +0100

    Supression d'un fichier, modifs sur les autres

--- include/nav.php
+++ include/nav.php
@@ -3,30 +3,21 @@
 <!-- Menu de navigation -->
 <nav class="navbar navbar-expand-lg navbar-fixed-top navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#">Jean Forteroche</a>
+        <a class="navbar-brand" href="index.php">Jean Forteroche</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Accueil</a>
+                    <a class="nav-link" href="index.php">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Livres</a>
+                    <a class="nav-link" href="index.php?action=romans">Romans</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Articles</a>
+                    <a class="nav-link" href="articles.php">Articles</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Profil</a>
-                </li>
-                <li class="nav-item">
-                    <!-- Si l'utilisateur est connecté en tant qu'admin alors on affiche "Administration" -->
-                    <?php if (isset($_SESSION["admin"]) AND $_SESSION["admin"] = 1 ){ ?>
-                    <a class="nav-link" href="#">Administration</a>
-                </li>
-                    <?php } ?>
             </ul>
         </div>
     </div>
