814f560cf33329bf09e5d3a4850752cbc3fbf91b
Raypea <batrob@sfr.fr>
2019-01-23 13:57:03 +0100

    Mise à jour de l'index

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <link rel="stylesheet" href="main.css">
-        <title>Blog - Jean Forteroche</title>
-    </head>
+     
+    <!-- HEADER PAGE D'ACCUEIL -->
+    <?php 
+    $pageTitle = 'Accueil - Jean Forteroche';
+    include("include/header.php"); ?>
+    
+    <!-- MENU SCROLL AVEC LA PAGE / HEADER -->
+    <?php include("include/nav.php"); ?>
 
-    <body>
-        <!-- MENU SCROLL AVEC LA PAGE -->
-        <?php include("include/nav.php"); ?>
+    <?php include("routeur.php");
+    $routeur = new Router();
+    $routeur->getPage(); ?>
 
-        
-    </body>
-</html>
+    <!-- FOOTER -->      
+    <?php include("include/footer.php"); ?>
