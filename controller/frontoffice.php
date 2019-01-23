c3f6ef86307c0e2849b6ca3025cb46c4ea065d25
Raypea <batrob@sfr.fr>
2019-01-23 13:48:36 +0100

    Adaptation du controller pour répondre aux attentes du routeur

--- controller/frontoffice.php
+++ controller/frontoffice.php
@@ -1,14 +1,21 @@
 <?php
 
-require('../model/frontoffice.php');
+require('model/Chapter.php');
 
-class Chapters {
+class FOController {
+    // On récupère la vue de la page d'accueil
+    public function getHome()
+    {
+        include("view/frontend/homeView.php"); 
+    }
+
     public function listChapters()
     {
-        $chapters = getChapters();
+        $model = new Chapter();
+        $chapters = $model->getChapters();
 
         // On appelle la vue correspondante
-        require('../view/frontend/listChaptersView.php');
+        require('view/frontend/chapterView.php');
     }
 
     public function chapter()
@@ -17,7 +24,7 @@ class Chapters {
         $comments = getComments($_GET['id']);
 
         // On appelle la vue correspondante
-        require('../view/frontend/chapterView.php');
+        require('view/frontend/chapterView.php');
     }
 
     // Ajout de commentaire - partie contrôleur
@@ -29,45 +36,9 @@ class Chapters {
             die('Impossible d\'ajouter le commentaire !');
         }
         else {
-            header('Location: index.php?action=chapter&id=' . $chapterId);
+            header('Location: romans.php?action=chapter&id=' . $chapterId);
         }
     }
 }
 
-
-class Connexion { 
-    public function Verification() {
-        if(!empty($_POST['email']) && isset($_POST['email']) && !empty($_POST['password']) && isset($_POST['password']))
-        {
-            $sql = "SELECT id, login, email, firstname, lastname, date_creation, id_groupe FROM users WHERE email = :email";
-            
-            $query = $db -> prepare($sql);
-            $query -> bindValue(":email", $_POST['email'], PDO::PARAM_STR);
-            $query -> execute();
-
-            $password_bdd = $query -> fetch();
-
-            if (!empty($password_bdd) AND sha1($_POST['password']) === $password_bdd['password']){
-
-            // Démarrage de session
-            session_start();
-            $_SESSION['id'] = htmlentities($password_bdd['id']);
-            $_SESSION['login'] = htmlentities($password_bdd['login']);
-            $_SESSION['email'] = htmlentities($password_bdd['email']);
-            $_SESSION['firstname'] = htmlentities($password_bdd['firstname']);
-            $_SESSION['lastname'] = htmlentities($password_bdd['lastname']);
-
-            header("Location: ../index.php");
-            }
-            else
-            {
-                header("Location: ../index.php");
-            }
-        }
-        else
-        {
-            
-        }
-    }
-}
 
