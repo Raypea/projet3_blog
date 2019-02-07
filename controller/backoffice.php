ea805e41e8bc53003d38413b101c2e5b9c522682
Raypea <45882074+Raypea@users.noreply.github.com>
2019-02-07 17:03:33 +0100

    Ajout de la fonction de suppression d'un chapitre

--- controller/backoffice.php
+++ controller/backoffice.php
@@ -1,8 +1,38 @@
 <?php
 require_once('model/Chapter.php');
-require_once('model/Comments.php');
-require_once('model/Admin.php');
 
 class BOController {
-    
+    // Suppression d'un chapitre partie contrôleur
+    public function supprChapterControl($chapterId)
+    {
+        $modelSupprChapter = new Chapter();
+
+        $result = $modelSupprChapter->supprChapter($chapterId);
+
+        if($result === false)
+        {
+            die('Impossible de supprimer le chapitre');
+        }
+        else 
+        {
+            header('Location: index.php?action=romans');
+        }
+    }
+
+    // Edition d'un chapitre partie contrôleur
+    public function editChapterControl($chapterId)
+    {
+        $modelEditChapter = new Chapter();
+
+        $result = $modelEditChapter->editChapter($chapterId);
+
+        if($result === false)
+        {
+            die('Impossible d\'éditer le chapitre');
+        }
+        else
+        {
+            header('Location: index.php?action=romans');
+        }
+    }
 }
