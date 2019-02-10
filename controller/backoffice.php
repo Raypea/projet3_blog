27a9573098ae9c884423b6bfb658ed33e34f0add
Raypea <45882074+Raypea@users.noreply.github.com>
2019-02-10 14:35:31 +0100

    Ajout de la suppression de commentaires et de signalement

--- controller/backoffice.php
+++ controller/backoffice.php
@@ -1,12 +1,13 @@
 <?php
 require_once('model/Chapter.php');
+require_once('model/Comments.php');
+require_once('model/Admin.php');
 
 class BOController {
     // Suppression d'un chapitre partie contrôleur
     public function supprChapterControl($chapterId)
     {
         $modelSupprChapter = new Chapter();
-
         $result = $modelSupprChapter->supprChapter($chapterId);
 
         if($result === false)
@@ -19,14 +20,58 @@ class BOController {
         }
     }
 
+    // Modération de commentaires partie contrôleur
+    public function moderateCommentControl()
+    {
+        $modelModerateComment = new Comments();
+ 
+        $moderatedComments = $modelModerateComment->moderateComments();
+
+        require('view/backend/moderateView.php');
+    }
+
+    // Suppression d'un signalement de commentaire - partie contrôleur
+    public function unsignalCommentControl($commentId)
+    {
+        $modelUnsignaledComment = new Comments();
+
+        $resultUnsignaledComment = $modelUnsignaledComment->unsignalComment($commentId);
+
+        if($resultUnsignaledComment === false)
+        {
+            die('Impossible d\'enlever le signalement du commentaire');
+        }
+        else
+        {
+            header('Location: index.php?action=moderer');
+        }
+    }
+
+    // Suppression d'un commentaire partie contrôleur
+    public function supprCommentControl($commentId)
+    {
+        $modelSupprComment = new Comments();
+
+        $resultComment = $modelSupprComment->supprComment($commentId);
+
+        if($resultComment === false)
+        {
+            die('Impossible de supprimer le commentaire');
+        }
+        else
+        {
+            header('Location: index.php?action=romans');
+        }
+    }
+
     // Edition d'un chapitre partie contrôleur
     public function editChapterControl($chapterId)
     {
         $modelEditChapter = new Chapter();
 
-        $result = $modelEditChapter->editChapter($chapterId);
+        $resultChapter = $modelEditChapter->editChapter($chapterId);
 
-        if($result === false)
+        if($resultChapter === false)
         {
             die('Impossible d\'éditer le chapitre');
         }
